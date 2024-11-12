fe0cb971923d643a0a1a2f186a14dc35cab816b1
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2024-11-12 20:50:02 -0300

    feat: tentativa 2

--- arquitetura/pub-sub/notifier-app/app.py
+++ arquitetura/pub-sub/notifier-app/app.py
@@ -1,12 +1,15 @@
 import json
 import logging
-import requests
+import telegram
+import asyncio
 from confluent_kafka import Consumer, KafkaError
 
+api_key = '7556072798:AAGI2xXjAUL1dJNZZ6-LGUYsagds70lak8M'
+user_id = '5464078318'
 
-TOKEN = '7556072798:AAGI2xXjAUL1dJNZZ6-LGUYsagds70lak8M'
-CHAT_ID = '5464078318'
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+async def send_telegram_message(message):
+    bot = telegram.Bot(token=api_key)
+    await bot.send_message(chat_id=user_id, text=message)
 
 c = Consumer({
     'bootstrap.servers': 'kafka1:19091,kafka2:19092,kafka3:19093',
@@ -19,38 +22,29 @@ c = Consumer({
 
 c.subscribe(['/notificacao'])
 
-
-def send_telegram_message(message):
-    payload = {
-        'chat_id': CHAT_ID,
-        'text': message
-    }
+def consume_and_notify():
     try:
-        response = requests.post(TELEGRAM_API_URL, data=payload)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Erro ao mandar mensagem pro telegram: {e}")
+        while True:
+            msg = c.poll(0.1)
+            if msg is None:
+                continue
+            elif not msg.error():
+                data = json.loads(msg.value())
+                filename = data['filename']
+                operation = data['operation']
 
+                notification_message = f'O arquivo {filename} foi {operation}.'
+                logging.info(f"Notificação: {notification_message}")
 
-try:
-    while True:
-        msg = c.poll(0.1)
-        if msg is None:
-            continue
-        elif not msg.error():
-            data = json.loads(msg.value())
-            filename = data['filename']
-            operation = data['operation']
+                asyncio.run(send_telegram_message(notification_message))
+            elif msg.error().code() == KafkaError._PARTITION_EOF:
+                logging.warning(f'End of partition reached: {msg.topic()} {msg.partition()}')
+            else:
+                logging.error(f'Error: {msg.error().str()}')
 
-            notification_message = f'O arquivo {filename} foi {operation}.'
-            send_telegram_message(notification_message)
-            logging.info(f"Notificação: {notification_message}")
-        elif msg.error().code() == KafkaError._PARTITION_EOF:
-            logging.warning(f'End of partition reached: {msg.topic()} {msg.partition()}')
-        else:
-            logging.error(f'Error: {msg.error().str()}')
+    except KeyboardInterrupt:
+        pass
+    finally:
+        c.close()
 
-except KeyboardInterrupt:
-    pass
-finally:
-    c.close()
+consume_and_notify()
